ec9148fd12a776d7889beb9b38f5db745271f2ae
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-26 03:26:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ var kittens = [] //define your array here
 // Add your functions and code here
 function destructivelyAppendKitten(name){
   kittens.push(name);
+  
 }
-function 
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  
+}
