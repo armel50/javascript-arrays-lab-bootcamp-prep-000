329dfeff81e210315ecb4559cf3985c6a2302ffe
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-26 03:28:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(){
   kittens.pop();
 }
-function 
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
